0f925453a61c33853363a40dd187b464d67401d9
Filipp Vynnykov <ynov07@rambler.ru>
2019-05-31 16:44:51 +0300

    Add project files.

---
+++ PeopleViewer.Common/IDataReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleViewer.Common
+{
+    public interface IDataReader
+    {
+        IEnumerable<Person> GetPeople();
+        Person GetPerson(int id);
+    }
+}
---
+++ PeopleViewer/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using PeopleViewer.Presentation;
+using PersonDataReader.Service;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            BuildDependencies();
+            Application.Current.MainWindow.Show();
+        }
+
+        private static void BuildDependencies()
+        {
+            var reader = new ServiceReader();
+            var viewModel = new PeopleViewModel(reader);
+            Application.Current.MainWindow = new PeopleViewerWindow(viewModel);
+        }
+    }
+}
